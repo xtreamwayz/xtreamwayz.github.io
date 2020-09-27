90dcc1667f89cf0017543944fab226ddd03dd683
Geert Eltink <geert.eltink@gmail.com>
2020-09-27 14:27:20 +0200

    feat: remove unused css

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: {
     autoprefixer: {},
+    '@fullhuman/postcss-purgecss': {
+      content: [
+        './content/**/*.md',
+        './layouts/**/*.html'
+      ],
+      whitelist: ['pre', 'code', 'blockquote']
+    }
   }
 };
